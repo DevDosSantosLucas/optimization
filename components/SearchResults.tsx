d40a5639f6f19989f0fb4e23111acaaebf50ac44
Lucas Ribeiro dos Santos <lucas1995ls@outlook.com>
2022-06-13 20:12:33 -0300

    react-virtualized

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
+import {List, ListRowRenderer} from 'react-virtualized' //Auto-size
 import { ProductItem } from "./ProductItem";
-import { useMemo} from 'react'
+
 
 interface SearchResultsProps {
   totalPrice: number;
@@ -20,17 +21,38 @@ export function SearchResults({results,onAddToWishlist,totalPrice}: SearchResult
   //   },0)
   // },[results])
 
+    const rowRenderer :ListRowRenderer =({index,key,style}) =>{
+      return(
+        <div key ={key} style={style}>
+        <ProductItem
+        product = {results[index]}
+        onAddToWishlist = {onAddToWishlist} 
+        />
+        </div>
+    
+      )
+    }
+
   return(
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product =>{
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount ={results.length}
+        rowRenderer={rowRenderer}
+
+      />
+      {/* {results.map(product =>{
         return(
           <ProductItem
             key={product.id}  
             product = {product}
             onAddToWishlist = {onAddToWishlist} />
         );
-      })}
+      })} */}
     </div>
   )
 }
